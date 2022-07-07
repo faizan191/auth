7a89b92a80df196254b8ec83e3c8010bf8be2f90
faizan191 <uddinfaizan13@gmail.com>
2022-07-07 22:52:27 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+//jshint esversion:6
+
+
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser')
+const app = express();
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+mongoose.connect('mongodb://localhost:27017/userDB',{useNewUrlParser: true});
+
+const userSchema = mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const secret = "thisianotagoodsecret";
+// adding plugin to the schema and encrypt only to password
+userSchema.plugin(encrypt,{secret:secret, encryptedFields:['password']});
+
+const User = mongoose.model('User',userSchema);
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.get('/login', (req, res) => {
+    res.render('login')
+});
+
+app.get('/register', (req, res) => {
+    res.render('register')
+});
+
+app.post('/register',async (req, res) => {
+    const {username,password} = req.body;
+     const newUser =  new User({
+        email:username,
+        password
+     })
+     await newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.render('secrets');
+        }
+     });
+});
+
+
+app.post('/login',  (req, res) => {
+    const {username,password} = req.body;
+
+    User.findOne({email:username}, function(err,foundUser) {
+        if(err){
+            console.log(err);
+            return res.send('Error')
+        }else{
+            if(foundUser){
+                if(foundUser.password === password);
+                res.render('secrets');
+            }else{
+                res.send('Error occoured')
+            }
+        }
+    })
+});
+
+
+
+app.listen(5001, () => {
+    console.log(`Server started on 5001`);
+});
