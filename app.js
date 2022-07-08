63203dab1d0a83a6cf60da6b33056cf60fa02f2f
faizan191 <uddinfaizan13@gmail.com>
2022-07-08 21:27:39 +0530

    Hashing

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require('ejs');
 const bodyParser = require('body-parser')
 const app = express();
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 mongoose.connect('mongodb://localhost:27017/userDB',{useNewUrlParser: true});
 
@@ -14,11 +14,6 @@ const userSchema = mongoose.Schema({
     email: String,
     password: String
 })
-
-console.log(process.env.API_KEY);
-
-// adding plugin to the schema and encrypt only to password
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:['password']});
 
 const User = mongoose.model('User',userSchema);
 
@@ -43,7 +38,8 @@ app.post('/register',async (req, res) => {
     const {username,password} = req.body;
      const newUser =  new User({
         email:username,
-        password
+        //using hash function
+        password:md5(password)
      })
      await newUser.save(function(err){
         if(err){
@@ -56,25 +52,28 @@ app.post('/register',async (req, res) => {
 
 
 app.post('/login',  (req, res) => {
-    const {username,password} = req.body;
-
+    const {username} = req.body;
+    console.log(req.body.password)
+    const password = md5(req.body.password)
+    console.log(username,password);
     User.findOne({email:username}, function(err,foundUser) {
         if(err){
             console.log(err);
             return res.send('Error')
+        }else if(foundUser){
+
+            console.log(foundUser);
+            console.log(foundUser.password);
+            if(foundUser.password == password);
+            res.render('secrets');
         }else{
-            if(foundUser){
-                if(foundUser.password === password);
-                res.render('secrets');
-            }else{
                 res.send('Error occoured')
             }
-        }
     })
 });
 
 
 
-app.listen(5001, () => {
-    console.log(`Server started on 5002`);
+app.listen(5000, () => {
+    console.log(`Server started on 5000`);
 });
